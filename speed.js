218e69b3516fd4b3fd03b1fd228f02ff4c45c0f4
KrasnovM <krasnov.m2000@gmail.com>
2023-08-31 19:39:38 +0300

    Update speed.js
    Comments changes

--- speed.js
+++ speed.js
@@ -5,26 +5,25 @@ var videoTimeDefault = document.getElementsByClassName("ytp-time-duration")[0].i
 
 videoPlayer.addEventListener('playing', e=>{ //add video speed identifier after duration when video starts playing
     var videoTimeDefaultTmp = document.getElementsByClassName("ytp-time-duration")[0].innerHTML
-    if(!videoTimeDefaultTmp.includes("x")) { //TODO: may be change innerHTML every time on playing? (but old code from interval works)
+    if(!videoTimeDefaultTmp.includes("x")) {
         videoTimeDefault = videoTimeDefaultTmp
-        videoTime.innerHTML = videoTimeDefault + " x" + videoPlayer.playbackRate
+        videoTime.innerHTML = videoTimeDefault + " x" + videoPlayer.playbackRate // add video speed identifier after video duration
     }
 })
 
 document.addEventListener('keydown', e=>{    //global event listener for 'keydown' events, 'e' is the key pressed
-  
+    
     if(e.keyCode === 221 && videoPlayer.playbackRate < 16) {  //key code 221 is for ']' button, 16 is the maximum speed for playbackRate property
-      
+        
         javascript:videoPlayer.playbackRate += 0.25  //0.25 is the increment to current speed; 1, 0.5, 0.25, 0.1 work fine; 
                                                      //if sum of increments can't be equal to 1 (2, 0.75, 0.33) they will go beyond 16 or below 0 
                                                      //additional validation will be required (video player won't crash, code just won't be executed)
-      
+        
         videoTime.innerHTML = videoTimeDefault + " x" + videoPlayer.playbackRate //change video speed identifier after video duration
-      
+        
     } else if (e.keyCode === 219 && videoPlayer.playbackRate > 0) { //key code 219 is for '[' button, 0 is the minimum speed for playbackRate property
-      
         javascript:videoPlayer.playbackRate -= 0.25 // same stuff but it's decrement
-        videoTime.innerHTML = videoTimeDefault + " x" + videoPlayer.playbackRate    
+        videoTime.innerHTML = videoTimeDefault + " x" + videoPlayer.playbackRate   
     }
     
     if(e.keyCode === 190 || e.keyCode === 188 && videoPlayer.playbackRate <= 2 && videoPlayer.playbackRate >= 0.25) { //video speed can be changed by hotkeys in player ('>'(190) and '<'(188)) but only between 0.25 and 2 
@@ -35,12 +34,16 @@ document.addEventListener('keydown', e=>{    //global event listener for 'keydow
 
 //some stuff for testing
 //javascript:document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate = 1;
+//2.5
+//3
+//3.5
+//4
+//4.5
+//5
+//+= 0.5
 //document.querySelector('video').playbackRate = 1.25
-//document.getElementsByClassName("video-stream html5-main-video")[0].setPlaybackRate = 0.25
 
 /*
-videoTime.innerHTML = videoTimeDefault + " x" + videoPlayer.playbackRate // add video speed identifier after video duration 
-
 setInterval(function() {  //if you click on another video without reloading page videoTimeDefault won't update, identifier will be added only after speed change
                           // periodic function will check every 20 seconds for changes
     var videoTimeDefaultTmp = document.getElementsByClassName("ytp-time-duration")[0].innerHTML
